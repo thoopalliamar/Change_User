45c18e2a518c99282879c19ce39c55e2504b45dc
Thoopalli Amarnath <40356920+thoopalliamar@users.noreply.github.com>
2018-10-11 22:17:28 +0530

    Update changerole.sh

--- changerole.sh
+++ changerole.sh
@@ -4,5 +4,5 @@ echo $HOST_NAME
 echo "Enter the name of the user"
 read USER_NAME
 echo $USER_NAME "CHANGING THIS TO ADMIN USER"
-aws iam attach-user-policy --user-name $USER_NAME --policy-arn arn:aws:iam::aws:policy/AlexaForBusinessDeviceSetup
+aws iam attach-user-policy --user-name $USER_NAME --policy-arn arn:aws:iam::aws:policy/AdministratorAccess
 echo "now the user is changed to admin congo" 
